b5b9822dc26f5ed5dd2c52494696e492fc6ce12d
Afua <afua.danquah@sky.uk>
2022-09-25 17:26:52 +0100

    created the spring boot application and boiler plate for private_status-controller

---
+++ retro-movies-app/src/main/java/sky/uk/retromoviesapp/RetroMoviesAppApplication.java
@@ -0,0 +1,13 @@
+package sky.uk.retromoviesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroMoviesAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RetroMoviesAppApplication.class, args);
+  }
+
+}
---
+++ retro-movies-app/src/main/java/sky/uk/retromoviesapp/controller/PrivateController.java
@@ -0,0 +1,4 @@
+package sky.uk.retromoviesapp.controller;
+
+public class PrivateController {
+}
