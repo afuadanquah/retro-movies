c9f7d695d37117e8a1abfd5e5f05a75227654590
Afua <afua.danquah@sky.uk>
2022-09-25 18:21:11 +0100

    feat: created the private/status endpoint with body of ok and response code of 200

--- retro-movies-app/src/main/java/sky/uk/retromoviesapp/controller/PrivateController.java
+++ retro-movies-app/src/main/java/sky/uk/retromoviesapp/controller/PrivateController.java
@@ -1,4 +1,17 @@
 package sky.uk.retromoviesapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/private")
 public class PrivateController {
+
+
+  @GetMapping("/status")
+  public ResponseEntity<String> getPrivateStatus(){
+    return new ResponseEntity<>("OK", HttpStatus.OK);
+  }
 }
